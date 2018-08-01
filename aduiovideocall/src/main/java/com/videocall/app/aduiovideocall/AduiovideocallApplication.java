b479b4bb3571e0755cd272a4b057be071036d8d6
sanu <sanu@sanu-PC>
2018-08-01 21:34:54 +0530

    first changes

---
+++ aduiovideocall/src/main/java/com/videocall/app/aduiovideocall/AduiovideocallApplication.java
@@ -0,0 +1,14 @@
+package com.videocall.app.aduiovideocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@SpringBootApplication
+
+public class AduiovideocallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AduiovideocallApplication.class, args);
+	}
+}
